cec407352454c9e7b6b62677a86250353a7a3943
Omar Alazaizeh <omx302@gmail.com>
2021-09-02 07:28:09 +0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Cell from "./components/cell.js";
 
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:4000"); // will need this
+const socket = io.connect(
+  process.env.REACT_APP_SERVER || "http://localhost:4000"
+); // will need this
 let arr = [];
 class App extends React.Component {
   constructor(props) {
